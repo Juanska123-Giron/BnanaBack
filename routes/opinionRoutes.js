0ae31512c08cbb6a3fafbe53a29e801ce4a8595b
JuanPaGiron <jpgm803@gmail.com>
2024-09-20 13:38:59 -0500

    initial setup

--- routes/opinionRoutes.js
+++ routes/opinionRoutes.js
@@ -1,22 +1,20 @@
 import express from "express";
 import {
-    obtenerOpinionesPorProducto,
-    nuevaOpinion,
-    obtenerOpinionesPorUsuario,
-    editarOpinion,
-    eliminarOpinion
+  obtenerOpinionesPorProducto,
+  nuevaOpinion,
+  obtenerOpinionesPorUsuario,
+  editarOpinion,
+  eliminarOpinion,
 } from "../controllers/opinionController.js";
 import checkAuth from "../middleware/checkAuth.js";
 const router = express.Router();
-router.route('/').post(checkAuth, nuevaOpinion);
+router.route("/").post(checkAuth, nuevaOpinion);
 router
-    .route('/:id')
-    .get(obtenerOpinionesPorProducto)
-    .put(checkAuth, editarOpinion)
-    .delete(checkAuth, eliminarOpinion)
-;
-//Agregar el cheachAuth
-router.route('/usuarioOpiniones/:id').get(checkAuth, obtenerOpinionesPorUsuario);
-
+  .route("/:id")
+  .get(obtenerOpinionesPorProducto)
+  .put(checkAuth, editarOpinion)
+  .delete(checkAuth, eliminarOpinion);
+//Agregar el checkAuth
+router.route("/usuarioOpiniones/:id").get(obtenerOpinionesPorUsuario);
 
 export default router;
